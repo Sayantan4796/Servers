49d815e0ba4477c3b1064ed8554104cc6e3b52d4
Sayantan4796 <cc19mtech11006@iith.ac.in>
2020-05-17 08:20:40 +0530

    added Cleint1.py

---
+++ Client1.py
@@ -0,0 +1,23 @@
+import socket
+
+
+host = socket.gethostname()               ############# as both code is running on same machine #########
+port = 5000                     ############ socket server port number ##############
+
+client_socket = socket.socket()  ############# initiate connection ##########
+client_socket.connect((host, port))         ######### connect to the server #############
+
+message = input(" -> ")            ########## take input ################
+
+while message.lower().strip() != 'bye':
+   client_socket.send(message.encode())            ########### send message ###########
+   data = client_socket.recv(1024).decode()          ########### receive response ############
+
+   print('Received from IIT- Hyderabad server: ' + data)  ############## show in terminal ############
+
+   message = input(" -> ")  # again take input
+
+client_socket.close()  # close the connection
+
+
+
